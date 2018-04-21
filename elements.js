833c810979804af9f25c9722d22d99da595d71e4
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-20 14:48:49 -1000

    add 404 to elements

--- elements.js
+++ elements.js
@@ -1,4 +1,20 @@
 let elements = {
+  404: `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Element not found!</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>404</h1>
+  <h2>Element not found!</h2>
+  <p>
+    <a href="/">back</a>
+  </p>
+</body>
+</html>`,
+
   helium: `<!DOCTYPE html>
 <html lang="en">
 <head>
