801ec44fb1a5c55cdaeb0a41181236f7c4d0dcff
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-20 18:32:31 -1000

    statusLine function

--- server.js
+++ server.js
@@ -2,19 +2,44 @@ const net = require('net');
 const elements = require('./elements');
 
 const server = net.createServer(function (request) {
+
   request.on('data', (data) => {
+    function setDate() {
+      let timeStamp = new Date().toUTCString();
+      return `Date: ${timeStamp}`;
+    }
+
     let strArr = data.toString().split(' ');
-    let timeStamp = new Date(Date.now()).toUTCString();
-    console.log(timeStamp);
+
+    function statusLine() {
+      let splitArr = strArr[2].split('\r');
+      let status = '';
+      if (elementsCheck === false) {
+        status = '404 Not Found';
+      } else {
+        status = '200 ok';
+      }
+      return `${splitArr[0]} ${status}`;
+    }
+
+    function makeHeader(){
+      let type = 'Content-Type: text/html; text/css';
+    }
+
     let uri = strArr[1]
-    if(uri === '/'){
+    if (uri === '/') {
+      let contentLength = elements['/index'].length;
       request.write(elements['/index']);
     }
+
     let elementsCheck = elements.hasOwnProperty(uri);
-    if (elementsCheck){
+
+    if (elementsCheck) {
       let contentLength = elements[uri].length;
-      console.log(contentLength);
-      request.write((elements[uri]));
+      request.write(elements[uri]);
+    } else {
+      let contentLength = elements['/notFound'].length;
+      request.write(elements["/notFound"]);
     }
     console.log(data.toString());
   })
